79217f6a23112ac4a6828a5277a25b3cece051b6
sajivddin <sajivddin.websolutions@gmail.com>
2025-09-09 17:05:21 +0530

    change

---
+++ lib/CustomerScreen/home.screen.dart
@@ -0,0 +1,207 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFFFFFFF),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Stack(
+            clipBehavior: Clip.none,
+            children: [
+              Container(
+                width: 393.w,
+                height: 260.h,
+                decoration: BoxDecoration(color: Color(0xFF006970)),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 50.h),
+                    Row(
+                      children: [
+                        SizedBox(width: 24.w),
+                        Container(
+                          width: 40.w,
+                          height: 40.h,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.grey,
+                          ),
+                          child: Image.asset(
+                            "assets/profile.png",
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                        Spacer(),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Row(
+                              children: [
+                                Text(
+                                  "Noida Sector 75",
+                                  style: GoogleFonts.inter(
+                                    fontSize: 13.sp,
+                                    fontWeight: FontWeight.w500,
+                                    color: Color(0xFFFFFFFF),
+                                  ),
+                                ),
+                                Icon(
+                                  Icons.keyboard_arrow_down_outlined,
+                                  color: Colors.white,
+                                  size: 22.sp,
+                                ),
+                              ],
+                            ),
+                            Text(
+                              "Golf city, Plot 8, Sector 75",
+                              style: GoogleFonts.inter(
+                                fontSize: 13.sp,
+                                fontWeight: FontWeight.w500,
+                                color: Color(0xFF99C3C6),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Spacer(),
+                        Container(
+                          width: 40.w,
+                          height: 40.h,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(color: Color(0xFF33878D)),
+                          ),
+                          child: Icon(
+                            Icons.notifications_none,
+                            color: Colors.white,
+                          ),
+                        ),
+                        SizedBox(width: 24.w),
+                      ],
+                    ),
+                    SizedBox(height: 25.h),
+                    Padding(
+                      padding: EdgeInsets.only(left: 24.w),
+                      child: Text(
+                        "Let’s Track your package",
+                        style: GoogleFonts.inter(
+                          fontSize: 18.sp,
+                          fontWeight: FontWeight.w400,
+                          color: Color(0xFFFFFFFF),
+                          letterSpacing: -1,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 15.h),
+                    Container(
+                      margin: EdgeInsets.only(left: 24.w, right: 24.w),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.only(
+                            left: 15.w,
+                            right: 15.w,
+                            top: 10.h,
+                            bottom: 10.h,
+                          ),
+                          filled: true,
+                          fillColor: Color(0xFFFFFFFF),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8.r),
+                            borderSide: BorderSide.none,
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8.r),
+                            borderSide: BorderSide.none,
+                          ),
+                          hintText: "Enter your tracking number",
+                          hintStyle: GoogleFonts.inter(
+                            fontSize: 14.sp,
+                            fontWeight: FontWeight.w400,
+                            color: Color(0xFFCCCCCC),
+                          ),
+                          prefixIcon: Icon(
+                            Icons.search,
+                            color: Color(0xFF2490A9),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                bottom: -28.h,
+                left: 24.w,
+                right: 24.w,
+                child: Container(
+                  width: 345.w,
+                  height: 90,
+                  decoration: BoxDecoration(
+                    color: Color(0xFFFFFFFF),
+                    borderRadius: BorderRadius.circular(8.r),
+                    border: Border.all(
+                      color: Color.fromARGB(51, 237, 237, 237),
+                      width: 1.w,
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 15,
+                        spreadRadius: 0,
+                        offset: Offset(0, 0),
+                        color: Color.fromARGB(15, 118, 118, 118),
+                      ),
+                    ],
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 24.w, right: 24.w),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        cardbuild("assets/SvgImage/check.svg", "Check Rate"),
+                        cardbuild("assets/SvgImage/pickup.svg", "Pick Up"),
+                        cardbuild("assets/SvgImage/drop.svg", "Drop Off"),
+                        cardbuild("assets/SvgImage/history.svg", "History"),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget cardbuild(String image, String name) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SvgPicture.asset(image),
+        SizedBox(height: 8.h),
+        Text(
+          name,
+          style: GoogleFonts.inter(
+            fontSize: 12.sp,
+            fontWeight: FontWeight.w500,
+            color: Color(0xFF353535),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/CustomerScreen/otp.screen.dart
+++ lib/CustomerScreen/otp.screen.dart
@@ -1,3 +1,4 @@
+import 'package:delivery_mvp_app/CustomerScreen/home.screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
@@ -17,161 +18,179 @@ class _OtpScreenState extends State<OtpScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFFFFFFF),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(height: 50.h),
-          Center(
-            child: Image.asset("assets/scooter.png", width: 84.w, height: 72.h),
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 24.w, right: 24.w),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(height: 28.h),
-                Text(
-                  "Enter the 4-digit code",
-                  style: GoogleFonts.inter(
-                    fontSize: 20.sp,
-                    fontWeight: FontWeight.w400,
-                    color: Color(0xFF111111),
-                    letterSpacing: -1,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 50.h),
+            Center(
+              child: Image.asset(
+                "assets/scooter.png",
+                width: 84.w,
+                height: 72.h,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 24.w, right: 24.w),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 28.h),
+                  Text(
+                    "Enter the 4-digit code",
+                    style: GoogleFonts.inter(
+                      fontSize: 20.sp,
+                      fontWeight: FontWeight.w400,
+                      color: Color(0xFF111111),
+                      letterSpacing: -1,
+                    ),
                   ),
-                ),
-                SizedBox(height: 4.h),
-                Text(
-                  "Please input  the verification code sent to your phone number 23480*******90",
-                  style: GoogleFonts.inter(
-                    fontSize: 12.sp,
-                    fontWeight: FontWeight.w400,
-                    color: Color(0xFF4F4F4F),
-                  ),
-                ),
-                TextButton(
-                  style: TextButton.styleFrom(
-                    minimumSize: Size(0, 30.h),
-                    padding: EdgeInsets.only(left: 0, top: 0),
-                    tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                  ),
-                  onPressed: () {},
-                  child: Text(
-                    "Change Number?",
+                  SizedBox(height: 4.h),
+                  Text(
+                    "Please input  the verification code sent to your phone number 23480*******90",
                     style: GoogleFonts.inter(
                       fontSize: 12.sp,
-                      fontWeight: FontWeight.w500,
-                      color: Color(0xFF006970),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 26.h),
-                OtpPinField(
-                  maxLength: 4,
-                  fieldHeight: 46.h,
-                  fieldWidth: 50.w,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  keyboardType: TextInputType.number,
-                  otpPinFieldStyle: OtpPinFieldStyle(
-                    textStyle: GoogleFonts.inter(
-                      fontSize: 24.sp,
-                      fontWeight: FontWeight.w500,
-                      color: Color(0xFF2D2D2D),
-                    ),
-                    activeFieldBackgroundColor: Color(0xFFF0F5F5),
-                    defaultFieldBackgroundColor: Color(0xFFF0F5F5),
-                    activeFieldBorderColor: Colors.transparent,
-                    defaultFieldBorderColor: Colors.transparent,
-                  ),
-                  otpPinFieldDecoration:
-                      OtpPinFieldDecoration.defaultPinBoxDecoration,
-
-                  onSubmit: (text) {},
-                  onChange: (text) {},
-                ),
-                SizedBox(height: 20.h),
-                Text.rich(
-                  TextSpan(
-                    text: "Didn’t get any code yet? ",
-                    style: GoogleFonts.inter(
-                      fontSize: 13.sp,
-                      fontWeight: FontWeight.w500,
+                      fontWeight: FontWeight.w400,
                       color: Color(0xFF4F4F4F),
                     ),
-                    children: [
-                      TextSpan(
-                        text: "Resend code",
-                        style: GoogleFonts.inter(
-                          fontSize: 13.sp,
-                          fontWeight: FontWeight.w500,
-                          color: Color(0xFF006970),
-                        ),
-                        recognizer: TapGestureRecognizer()..onTap = () {},
+                  ),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      minimumSize: Size(0, 30.h),
+                      padding: EdgeInsets.only(left: 0, top: 0),
+                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    ),
+                    onPressed: () {},
+                    child: Text(
+                      "Change Number?",
+                      style: GoogleFonts.inter(
+                        fontSize: 12.sp,
+                        fontWeight: FontWeight.w500,
+                        color: Color(0xFF006970),
                       ),
-                    ],
-                  ),
-                ),
-                SizedBox(height: 30.h),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: Size(327.w, 50.h),
-                    backgroundColor: Color(0xFF006970),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.r),
-                      side: BorderSide.none,
                     ),
                   ),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      CupertinoPageRoute(builder: (context) => OtpScreen()),
-                    );
-                  },
-                  child: Text(
-                    "Login",
-                    style: GoogleFonts.inter(
-                      fontSize: 15.sp,
-                      fontWeight: FontWeight.w500,
-                      color: Color(0xFFFFFFFF),
+                  SizedBox(height: 26.h),
+                  OtpPinField(
+                    maxLength: 4,
+                    fieldHeight: 46.h,
+                    fieldWidth: 50.w,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    keyboardType: TextInputType.number,
+                    otpPinFieldStyle: OtpPinFieldStyle(
+                      textStyle: GoogleFonts.inter(
+                        fontSize: 24.sp,
+                        fontWeight: FontWeight.w500,
+                        color: Color(0xFF2D2D2D),
+                      ),
+                      activeFieldBackgroundColor: Color(0xFFF0F5F5),
+                      defaultFieldBackgroundColor: Color(0xFFF0F5F5),
+                      activeFieldBorderColor: Colors.transparent,
+                      defaultFieldBorderColor: Colors.transparent,
+                    ),
+                    otpPinFieldDecoration:
+                        OtpPinFieldDecoration.defaultPinBoxDecoration,
+
+                    onSubmit: (text) {},
+                    onChange: (text) {},
+                  ),
+                  SizedBox(height: 20.h),
+                  Text.rich(
+                    TextSpan(
+                      text: "Didn’t get any code yet? ",
+                      style: GoogleFonts.inter(
+                        fontSize: 13.sp,
+                        fontWeight: FontWeight.w500,
+                        color: Color(0xFF4F4F4F),
+                      ),
+                      children: [
+                        TextSpan(
+                          text: "Resend code",
+                          style: GoogleFonts.inter(
+                            fontSize: 13.sp,
+                            fontWeight: FontWeight.w500,
+                            color: Color(0xFF006970),
+                          ),
+                          recognizer: TapGestureRecognizer()..onTap = () {},
+                        ),
+                      ],
                     ),
                   ),
-                ),
-                SizedBox(height: 7.h),
-                // Center(
-                //   child: Text.rich(
-                //     TextSpan(
-                //       text: "Need an account? ",
-                //       style: GoogleFonts.inter(
-                //         fontSize: 14.sp,
-                //         fontWeight: FontWeight.w400,
-                //         color: Color(0xFF4F4F4F),
-                //       ),
-                //       children: [
-                //         TextSpan(
-                //           text: "Sign up",
-                //           style: GoogleFonts.inter(
-                //             fontSize: 16.sp,
-                //             fontWeight: FontWeight.w500,
-                //             color: Color(0xFF006970),
-                //           ),
-                //           recognizer: TapGestureRecognizer()
-                //             ..onTap = () {
-                //               Navigator.push(
-                //                 context,
-                //                 CupertinoPageRoute(
-                //                   builder: (_) => RegisterScreen(),
-                //                 ),
-                //               );
-                //             },
-                //         ),
-                //       ],
-                //     ),
-                //   ),
-                // ),
-                SizedBox(height: 10.h),
-              ],
+                  SizedBox(height: 30.h),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(327.w, 50.h),
+                      backgroundColor: Color(0xFF006970),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.r),
+                        side: BorderSide.none,
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        CupertinoPageRoute(builder: (context) => HomeScreen()),
+                      );
+                    },
+                    child: Text(
+                      "Verify",
+                      style: GoogleFonts.inter(
+                        fontSize: 15.sp,
+                        fontWeight: FontWeight.w500,
+                        color: Color(0xFFFFFFFF),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 25.h),
+                  Center(
+                    child: Text(
+                      "By signing up, you agree to snap",
+                      style: GoogleFonts.inter(
+                        fontSize: 12.sp,
+                        fontWeight: FontWeight.w400,
+                        color: Color(0xFF111111),
+                      ),
+                    ),
+                  ),
+                  Center(
+                    child: Text.rich(
+                      TextSpan(
+                        text: "Terms of Service ",
+                        style: GoogleFonts.inter(
+                          fontSize: 12.sp,
+                          fontWeight: FontWeight.w400,
+                          color: Color(0xFF111111),
+                          decoration: TextDecoration.underline,
+                        ),
+                        children: [
+                          TextSpan(
+                            text: "and",
+                            style: GoogleFonts.inter(
+                              fontSize: 12.sp,
+                              fontWeight: FontWeight.w400,
+                              color: Color(0xFF111111),
+                              decoration: TextDecoration.none,
+                            ),
+                          ),
+                          TextSpan(
+                            text: " Privacy Policy.",
+                            style: GoogleFonts.inter(
+                              fontSize: 12.sp,
+                              fontWeight: FontWeight.w400,
+                              color: Color(0xFF111111),
+                              decoration: TextDecoration.underline,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10.h),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
