5b74e5b6f46e686fc440ccf665fcd0ebf4192fba
sajivddin <sajivddin.websolutions@gmail.com>
2025-09-12 11:02:06 +0530

    change

--- lib/CustomerScreen/home.screen.dart
+++ lib/CustomerScreen/home.screen.dart
@@ -49,144 +49,211 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFFFFFFF),
-      body: Stack(
-        children: [
-          Container(
-            width: 440,
-            height: 1542,
-            clipBehavior: Clip.antiAlias,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment(0.50, -0.00),
-                end: Alignment(0.50, 1.00),
-                colors: [const Color(0xFF82ECF3), Colors.white],
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Container(
+              width: 440.w,
+              height: 848.h,
+              clipBehavior: Clip.antiAlias,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment(0.50, -0.00),
+                  end: Alignment(0.50, 1.00),
+                  colors: [const Color(0xFF82ECF3), Colors.white],
+                ),
               ),
             ),
-          ),
-          SvgPicture.asset("assets/SvgImage/bg.svg"),
-          Positioned(
-            top: 40.h,
-            left: 24.w,
-            right: 24.w,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    Container(
-                      width: 40.w,
-                      height: 40.h,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.white,
-                      ),
-                      child: Icon(Icons.location_on, color: Color(0xFF006970)),
-                    ),
-                    SizedBox(width: 10.w),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Noida Sector 75",
-                          style: GoogleFonts.inter(
-                            fontSize: 13.sp,
-                            fontWeight: FontWeight.w500,
-                            color: Color(0xFF000000),
-                          ),
+            SvgPicture.asset("assets/SvgImage/bg.svg"),
+            Positioned(
+              top: 40.h,
+              left: 24.w,
+              right: 24.w,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        width: 40.w,
+                        height: 40.h,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.white,
                         ),
-                        Text(
-                          "Golf city, Plot 8, Sector 75",
-                          style: GoogleFonts.inter(
-                            fontSize: 12.sp,
-                            fontWeight: FontWeight.w400,
-                            color: Color(0xFF086E86),
-                          ),
+                        child: Icon(
+                          Icons.location_on,
+                          color: Color(0xFF006970),
                         ),
-                      ],
-                    ),
-                    Spacer(),
-                    Container(
-                      width: 40.w,
-                      height: 40.h,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.white,
                       ),
-                      child: Icon(
-                        Icons.notifications_none,
-                        color: Color(0xFF242126),
+                      SizedBox(width: 10.w),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Noida Sector 75",
+                            style: GoogleFonts.inter(
+                              fontSize: 13.sp,
+                              fontWeight: FontWeight.w500,
+                              color: Color(0xFF000000),
+                            ),
+                          ),
+                          Text(
+                            "Golf city, Plot 8, Sector 75",
+                            style: GoogleFonts.inter(
+                              fontSize: 12.sp,
+                              fontWeight: FontWeight.w400,
+                              color: Color(0xFF086E86),
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 16.h),
-                TextField(
-                  decoration: InputDecoration(
-                    contentPadding: EdgeInsets.only(
-                      left: 15.w,
-                      right: 15.w,
-                      top: 10.h,
-                      bottom: 10.h,
-                    ),
-                    filled: true,
-                    fillColor: Color(0xFFFFFFFF),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(40.r),
-                      borderSide: BorderSide.none,
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(40.r),
-                      borderSide: BorderSide.none,
-                    ),
-                    hintText: "Enter your tracking number",
-                    hintStyle: GoogleFonts.inter(
-                      fontSize: 14.sp,
-                      fontWeight: FontWeight.w400,
-                      color: Color(0xFFCCCCCC),
-                    ),
-                    prefixIcon: Icon(Icons.search, color: Color(0xFF2490A9)),
-                  ),
-                ),
-                SizedBox(height: 16.h),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Color(0xFFFFFFFF),
-                    borderRadius: BorderRadius.circular(20.r),
-                    border: Border.all(
-                      color: Color.fromARGB(51, 237, 237, 237),
-                      width: 1.w,
-                    ),
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: 15,
-                        spreadRadius: 0,
-                        offset: Offset(0, 0),
-                        color: Color.fromARGB(15, 118, 118, 118),
+                      Spacer(),
+                      Container(
+                        width: 40.w,
+                        height: 40.h,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.white,
+                        ),
+                        child: Icon(
+                          Icons.notifications_none,
+                          color: Color(0xFF242126),
+                        ),
                       ),
                     ],
                   ),
-                  child: Padding(
-                    padding: EdgeInsets.only(
-                      left: 24.w,
-                      right: 24.w,
-                      top: 16.h,
-                      bottom: 16.h,
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        cardbuild("assets/SvgImage/chec.svg", "Check Rate"),
-                        cardbuild("assets/SvgImage/picku.svg", "Pick Up"),
-                        cardbuild("assets/SvgImage/dro.svg", "Drop Off"),
-                        cardbuild("assets/SvgImage/his.svg", "History"),
-                      ],
+                  SizedBox(height: 16.h),
+                  TextField(
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.only(
+                        left: 15.w,
+                        right: 15.w,
+                        top: 10.h,
+                        bottom: 10.h,
+                      ),
+                      filled: true,
+                      fillColor: Color(0xFFFFFFFF),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(40.r),
+                        borderSide: BorderSide.none,
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(40.r),
+                        borderSide: BorderSide.none,
+                      ),
+                      hintText: "Enter your tracking number",
+                      hintStyle: GoogleFonts.inter(
+                        fontSize: 14.sp,
+                        fontWeight: FontWeight.w400,
+                        color: Color(0xFFCCCCCC),
+                      ),
+                      prefixIcon: Icon(Icons.search, color: Color(0xFF2490A9)),
                     ),
                   ),
-                ),
-              ],
+                  SizedBox(height: 16.h),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Color(0xFFFFFFFF),
+                      borderRadius: BorderRadius.circular(20.r),
+                      border: Border.all(
+                        color: Color.fromARGB(51, 237, 237, 237),
+                        width: 1.w,
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 15,
+                          spreadRadius: 0,
+                          offset: Offset(0, 0),
+                          color: Color.fromARGB(15, 118, 118, 118),
+                        ),
+                      ],
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.only(
+                        left: 24.w,
+                        right: 24.w,
+                        top: 16.h,
+                        bottom: 16.h,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          cardbuild("assets/SvgImage/chec.svg", "Check Rate"),
+                          cardbuild("assets/SvgImage/picku.svg", "Pick Up"),
+                          cardbuild("assets/SvgImage/dro.svg", "Drop Off"),
+                          cardbuild("assets/SvgImage/his.svg", "History"),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 28.h),
+                  GridView.builder(
+                    itemCount: myList.length,
+                    padding: EdgeInsets.zero,
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 20.w,
+                      mainAxisSpacing: 20.w,
+                      childAspectRatio: 0.80,
+                    ),
+                    itemBuilder: (context, index) {
+                      return Container(
+                        width: 158.w,
+                        height: 181.h,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8.r),
+                          border: Border.all(color: Color(0xFFE8E8E8)),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(height: 30.h),
+                            Center(
+                              child: SvgPicture.asset(
+                                //"assets/SvgImage/truc.svg",
+                                myList[index]['image'],
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(left: 9.w, top: 4.h),
+                              child: Text(
+                                // "Trucks",
+                                myList[index]['name'],
+                                style: GoogleFonts.inter(
+                                  fontSize: 18.sp,
+                                  fontWeight: FontWeight.w700,
+                                  color: Color(0xFF000000),
+                                  letterSpacing: -1,
+                                ),
+                              ),
+                            ),
+                            if (index == 0 || index == 1)
+                              Padding(
+                                padding: EdgeInsets.only(left: 9.w),
+                                child: Text(
+                                  //  "Choose from Our Fleet",
+                                  myList[index]['title'],
+                                  style: GoogleFonts.inter(
+                                    fontSize: 14.sp,
+                                    fontWeight: FontWeight.w400,
+                                    color: Color(0xFF000000),
+                                    letterSpacing: -1,
+                                  ),
+                                ),
+                              ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
 
       // body: SingleChildScrollView(
